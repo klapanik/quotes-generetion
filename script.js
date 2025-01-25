1a86485836969be8a6d5b0c8a47d21ed1074d8fa
Kostik Lapanik <lapanik.kostik@gmail.com>
2025-01-25 13:52:20 +0300

    fit: add data base

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     let quot = document.querySelector('#quotes');
     let author = document.querySelector('#author');
-    let button = document.querySelector('.generate-button')
-    let loadingQuotes = document.querySelector('.loading-quotes')
-    let quotesDataContent = document.querySelector('.quotes')
+    let buttonElement = document.querySelector('.generate-button');
+    let loadingQuotes = document.querySelector('.loading-quotes');
+    let quotesDataContent = document.querySelector('.quotes');
 
     let quoteList = [{
         name: 'Bjarne Stroustrup',
@@ -34,24 +34,111 @@ document.addEventListener('DOMContentLoaded', () => {
         quot: 'A low-level language is when attention is required to things that have nothing to do with programs in that language.'
     }]
 
-    const DEFAULT_QOUT_VALUE = quoteList[0].quot
-    const DEFAULT_AUTHOR_VALUE = quoteList[0].name
+    const dbName = 'QuotesDB';
+    const dbVersion = 5;
+    const request = window.indexedDB.open(dbName, dbVersion);
+    let objectStore;
+    let db;
 
-    quot.innerText = DEFAULT_QOUT_VALUE
-    author.innerText = DEFAULT_AUTHOR_VALUE
+    request.onerror = (event) => {
+        console.warn('Error in request: ', event.target.error)
+    }
 
-    button.addEventListener('click', () => {
-        loadingQuotes.style.display = 'block'
-        quotesDataContent.replaceWith(loadingQuotes)
+    request.onsuccess = (event) => {
+        db = event.target.result;
+        console.log('request onsuccess')
 
-        setTimeout(() => {
-            let counter = Math.floor(Math.random() * quoteList.length)
-            
-            quot.innerText = quoteList[counter].quot
-            author.innerText = quoteList[counter].name
+        const transaction = makeNewTX('quotesStore', 'readonly')
+        const store = transaction.objectStore('quotesStore')
 
-            loadingQuotes.replaceWith(quotesDataContent)
-            loadingQuotes.style.display = 'none'
-        }, 1500)
-    })
+        // let getReq = store.getAll()
+
+        // getReq.onsuccess = (event) => {
+        //     let request = event.target;
+
+        //     quot.innerText = request.result[0].quot;
+        //     author.innerText = request.result[0].name;
+        // }
+
+        let getReq = store.get(1) // тут вопросик есть у меня 
+
+        getReq.onsuccess = (event) => {
+            console.log('getRequest was succassfully done')
+            let request = event.target;
+
+            quot.innerText = request.result.quot;
+            author.innerText = request.result.name;
+        }
+        getReq.onerror = (event) => {
+            console.warn('Error in getRequest: ', event.target.error)
+        }
+    }
+
+    request.onupgradeneeded = (event) => {
+        db = event.target.result;
+        console.log('request onupgradeneeded')
+
+        if (!db.objectStoreNames.contains('quotesStore')) {
+            objectStore = db.createObjectStore('quotesStore', { keyPath: 'id' });
+        }
+
+        quoteList.forEach((quote) => {
+            objectStore.add(quote);
+        });
+    };
+
+    function generateNewQuot() {
+        const transaction = makeNewTX('quotesStore', 'readonly');
+        const store = transaction.objectStore('quotesStore');
+
+        const request = store.count(); // я тут чуть-чуть не понял 
+
+        console.log(request);
+
+        request.onsuccess = (event) => {
+            const count = event.target.result;
+            const randomIndex = Math.floor(Math.random() * count)
+            console.log(randomIndex)
+
+            let getReq = store.get(randomIndex + 1)
+
+            getReq.onsuccess = (event) => {
+                console.log('generateNewQuot getRequest was succassfully done')
+                let request = event.target;
+
+                loadingQuotes.style.display = 'block'
+                quotesDataContent.replaceWith(loadingQuotes)
+
+                setTimeout(() => {
+                    quot.innerText = request.result.quot;
+                    author.innerText = request.result.name;
+
+                    loadingQuotes.replaceWith(quotesDataContent)
+                    loadingQuotes.style.display = 'none'
+                }, 1500)
+            }
+            getReq.onerror = (event) => {
+                console.warn('Error in generateNewQuot getRequest: ', event.target.error)
+            }
+        }
+
+        request.onerror = (event) => {
+            console.warn('Error in generateNewQuot request: ', event.target.error)
+        }
+    }
+
+    function makeNewTX(storeName, mode) {
+        let tx = db.transaction(storeName, mode)
+
+        tx.oncomplete = () => {
+            console.log('transaction oncomplete')
+        }
+        tx.onerror = (event) => {
+            console.warn('Error in transaction: ', event.target.error)
+        }
+
+        return tx
+    }
+
+    buttonElement.addEventListener('click', generateNewQuot)
 })
